ec3933f1fa9ca2068b13d575043201b93f63ba2d
Michael <mjsvh99@gmail.com>
2022-10-03 22:01:56 -0300

    "agrego admin en config"

--- backend/config.ts
+++ backend/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default {
     MONGO_DATABASE: process.env.MONGO_DATABASE || 'test',
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    MONGO_USER: process.env.MONGO_USER || 'admin',
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'admin',
     PORT: process.env.PORT || '3001',
 }
